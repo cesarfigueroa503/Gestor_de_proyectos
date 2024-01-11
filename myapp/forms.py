09c7898288cee1202e879ffec98342fef1753109
cesarfigueroa503 <cesarfigueroa503@gmail.com>
2024-01-11 10:36:35 -0600

    correccion de formulario createTask

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,14 +5,16 @@ class CreateNewTask(forms.Form ):
     tittle = forms.CharField(label="Titulo de la tara", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
     description  = forms.CharField(label="Descripcion de la Tarea",widget=forms.Textarea(attrs={'class': 'input'}))
     
-    projects_list = list(Project.objects.all())
-    projects = []
+    def __init__(self, *args, **kwargs):
+        super(CreateNewTask, self).__init__(*args, **kwargs)
+            
+        projects_list = list(Project.objects.all())
+        projects = []
 
-    for i in projects_list:
-        projects.append((i.id, i.name))
+        for i in projects_list:
+            projects.append((i.id, i.name))
 
-
-    project = forms.ChoiceField(choices=projects, widget=forms.Select(attrs={'class':'input'}))
+        self.fields['project'] = forms.ChoiceField(choices=projects, widget=forms.Select(attrs={'class': 'input'}))
 
 class CreateNewProject(forms.Form):
     name = forms.CharField(label="Nombre del Proyecto", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
